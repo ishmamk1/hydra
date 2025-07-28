9c7d48f4dc96b4061220fa305092ceb33303e4ce
ishmamk1 <ishmamkhan1010@gmail.com>
2025-07-28 00:03:13 -0400

    clean constants.ts

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,10 +1,2 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCab4wloi0rJi3Oq2DFhvbbjLwlEUklAaE",
-    authDomain: "hydra-30de2.firebaseapp.com",
-    projectId: "hydra-30de2",
-    storageBucket: "hydra-30de2.firebasestorage.app",
-    messagingSenderId: "110507320417",
-    appId: "1:110507320417:web:795221ad3a63fce737b620",
-    measurementId: "G-J8TB6L90L1"
-};
+
 
